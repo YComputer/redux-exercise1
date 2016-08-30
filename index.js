e2a501a87a5851238ced10bb4cfc5a0e642ca9ff
YComputer <thea@163.com>
2016-08-30 10:16:00 +0800

    lesson8

--- index.js
+++ index.js
@@ -1,58 +1,49 @@
 var expect = require('expect');
 var redux = require('redux');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const counter = (state = 0, action) => {
-    switch (action.type){
+    switch (action.type) {
         case 'INCREMENT':
-            return state+1;
+            return state + 1;
         case 'DECRMENT':
-            return state-1;
+            return state - 1;
         default:
             return state;
     }
 }
 
-// const {createStore} = redux;
-const createStore = (reducer) => {
-    let state;
-    let listeners = [];
+const Counter = ({
+    value,
+    onIncrement,
+    onDecrement
+}) => (
+        <div>
+            <h1>{value}</h1>
+            <button onClick={onIncrement}>+</button>
+            <button onClick={onDecrement}>-</button>
+        </div>
+    );
 
-    const getState = () => {
-        return state;
-    };
-
-    const dispatch = (action) => {
-        state = reducer(state, action);
-        listeners.forEach(listener => listener());
-    };
-
-    const subscribe = (listener) => {
-        listeners.push(listener);
-            console.log('listeners',listeners.length)
-        
-        return () => {
-            listeners = listeners.filter(l => l !== listener);
-            console.log('listeners',listeners.length)
-        }
-    };
-
-    dispatch({});
-
-    return {getState, dispatch, subscribe};
-}
-
-
-
+const {createStore} = redux;
 const store = createStore(counter);
 
 const render = () => {
-     document.body.innerText = store.getState();
+    ReactDOM.render(
+        <Counter
+            value={store.getState() }
+            onIncrement={() =>
+                store.dispatch({ type: 'INCREMENT' })
+            }
+            onDecrement={() =>
+                store.dispatch({ type: 'DECRMENT' })
+            }
+            />,
+        document.getElementById('app')
+    )
 }
 
 store.subscribe(render);
-
 render();
 
-document.addEventListener('click', ()=>{
-    store.dispatch({type: 'INCREMENT'})
-})
